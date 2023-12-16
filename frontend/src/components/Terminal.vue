6cb783ef7d932e23477c3b818dc586aedbf00f4b
Louis Lam <louislam@users.noreply.github.com>
2023-12-16 17:50:54 +0800

    Change to @xterm/xterm

--- frontend/src/components/Terminal.vue
+++ frontend/src/components/Terminal.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { Terminal } from "xterm";
+import { Terminal } from "@xterm/xterm";
 import { FitAddon } from "@xterm/addon-fit";
 import { WebLinksAddon } from "xterm-addon-web-links";
 import { TERMINAL_COLS, TERMINAL_ROWS } from "../../../backend/util-common";
